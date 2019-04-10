d41d0fecbf1e320ccc71d0958b184c3c524acc82
Ahmed Tarek <pushqrdx@gmail.com>
2019-04-10 19:10:08 +0200

    using imba directly

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
-module.exports = function (bundler) {
-	bundler.addAssetType('imba', require.resolve('./asset.js'));
-};
+module.exports = function(bundler) {
+	bundler.addAssetType('imba', require.resolve('./asset.js'))
+}
